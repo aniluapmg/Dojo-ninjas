e6eb1563bfa999c9f8cb798fe1432a6cc80cb158
Paulina Muñoz <paulinamunozg@gmail.com>
2021-10-29 11:41:14 -0300

    Borrar un archivo_prueba

--- app/models/dojo.rb
+++ app/models/dojo.rb
@@ -1,7 +1,7 @@
 class Dojo < ApplicationRecord
   validates :state, presence: true, length: { is: 2 }
 
-  has_many :ninjas
+  has_many :ninjas, :dependent => :destroy
 
   after_destroy :destroy_all_ninjas_from_dojo
 
--- app/models/ninja.rb
+++ app/models/ninja.rb
@@ -1,4 +1,10 @@
 class Ninja < ApplicationRecord
   belongs_to :dojo
   validates :first_name, :last_name, presence: true
+
+  after_create :creacion_exitosa
+
+  def creacion_exitosa
+    p "Tu ninja se ha creado"
+  end
 end
